2244756c12792bfef13427fe8cc83e3732dfbe4f
DmitriySokolov2023 <sokolovoff2023@gmail.com>
2025-02-27 18:48:37 +0300

    add-base-rout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,22 @@
-import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 import { ProfileCardComponent } from './common-ui/profile-card/profile-card.component';
+import { Profile } from './data/interfaces/profile.interface';
 import { ProfileService } from './data/services/profile.service';
 
 @Component({
   selector: 'app-root',
-  imports: [ProfileCardComponent, CommonModule],
+  imports: [ProfileCardComponent, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   profileService = inject(ProfileService);
-  profiles = [];
+  profiles: Profile[] = [];
 
   constructor() {
     this.profileService.getTestAccounts().subscribe((val) => {
-      console.log(val);
+      this.profiles = val;
     });
   }
 }
